820313217009e8dd0fcce50b591ce111a238c12c
Jonathan Guyer <guyer@nist.gov>
2017-01-14 22:55:27 -0500

    Add Windows build script

---
+++ recipe/bld.bat
@@ -0,0 +1,24 @@
+mkdir build
+cd build
+
+:: Configure.
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -D ENABLE_OS_SPECIFIC_INSTALL=OFF ^
+      -D ENABLE_MATCH=OFF ^
+      -D ENABLE_PETSC=OFF ^
+      -D ENABLE_SLEPC=OFF ^
+      %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+nmake
+if errorlevel 1 exit 1
+
+:: Test.
+ctest
+if errorlevel 1 exit 1
+
+:: Install.
+nmake install
+if errorlevel 1 exit 1
