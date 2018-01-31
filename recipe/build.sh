4a14f62521f2c4442e94edb1f76cd65f220758a6
Jonathan Guyer <guyer@nist.gov>
2018-01-31 15:07:48 -0500

    Remove unused cmake variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# see http://conda.pydata.org/docs/build.html for hacking instructions.
 
 # unpack.
 mkdir build
@@ -9,7 +8,6 @@ cd build
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DENABLE_OS_SPECIFIC_INSTALL=OFF \
-    -DENABLE_MATCH=OFF \
     -DENABLE_PETSC=OFF \
     -DENABLE_SLEPC=OFF \
     .. | tee cmake.log 2>&1
