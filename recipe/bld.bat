2710d7c1e776f92d3fa67e8d9ab1fe4d141f5f01
Jonathan Guyer <guyer@nist.gov>
2018-06-29 07:38:23 -0400

    Attempt to replicate `windows_official` builds
    Previously was trying to do `windows64_msvc_ci` from
    https://gitlab.onelab.info/gmsh/gmsh/blob/master/.gitlab-ci.yml

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,19 +2,26 @@ mkdir build
 cd build
 
 :: Configure.
-cmake -G "NMake Makefiles" ^
-      -D BLAS_LAPACK_LIBRARIES=%PREFIX%\Library\lib ^
+cmake -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -D ENABLE_OS_SPECIFIC_INSTALL=ON ^
+      -D ENABLE_MATCH=OFF ^
+      -D ENABLE_PETSC=OFF ^
+      -D ENABLE_SLEPC=OFF ^
+      -D CMAKE_C_COMPILER=/usr/bin/mingw32-gcc.exe ^
+      -D CMAKE_CXX_COMPILER=/usr/bin/mingw32-g++.exe ^
+      -D CMAKE_Fortran_COMPILER=/usr/bin/mingw32-gfortran.exe ^
+      -D CMAKE_RC_COMPILER=/usr/bin/mingw32-windres.exe ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: Build.
-nmake
+make package
 if errorlevel 1 exit 1
 
 :: Test.
-ctest
+ctest -D Experimental --output-on-failure
 if errorlevel 1 exit 1
 
 :: Install.
-nmake install
+make install
 if errorlevel 1 exit 1
