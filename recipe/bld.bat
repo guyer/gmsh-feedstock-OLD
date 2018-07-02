1a48eb6d62c9e9197d5201b3228ee7643b1e897b
Jonathan Guyer <guyer@nist.gov>
2018-07-02 10:47:17 -0400

    Fix CMAKE_INSTALL_PREFIX
    For unintelligible reasons, executables get installed in <env>\Library\bin\
    on Windows and we were putting them in <env>\bin

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,15 @@
+:: Needed so we can find stdint.h from msinttypes.
+set LIB=%LIBRARY_LIB%;%LIB%
+set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
+set INCLUDE=%LIBRARY_INC%;%INCLUDE%
+
 mkdir build
 cd build
 
 :: Configure.
 cmake -G "NMake Makefiles" ^
-      -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D ENABLE_OS_SPECIFIC_INSTALL=OFF ^
-      -D ENABLE_MATCH=OFF ^
       -D ENABLE_PETSC=OFF ^
       -D ENABLE_SLEPC=OFF ^
       %SRC_DIR%
